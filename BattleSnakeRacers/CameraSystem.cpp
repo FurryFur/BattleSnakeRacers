941b28eec9c2575f78bfe51acc9e1ba7152ab9de
FurryFur <lance.e.chaney@gmail.com>
2018-05-05 19:39:45 +1200

    Fixed tilting. Not sure if correct still.

--- BattleSnakeRacers/CameraSystem.cpp
+++ BattleSnakeRacers/CameraSystem.cpp
@@ -1,6 +1,7 @@
 #include "CameraSystem.h"
 
 #include "Entity.h"
+#include "RenderSystem.h"
 
 #include <glm\glm.hpp>
 
@@ -30,19 +31,31 @@ void CameraSystem::update()
 			const vec3& playerPos = m_playerList[0]->transform.position;
 			vec3 topLeft = playerPos;
 			vec3 bottomRight = topLeft;
+			vec3 topRight = topLeft;
+			vec3 bottomLeft = topLeft;
 			for (auto player : m_playerList) {
 				const vec3& playerPos = player->transform.position;
 				topLeft = glm::min(topLeft, playerPos);
 				bottomRight = glm::max(bottomRight, playerPos);
-				bottomRight.y = topLeft.y; // Ensure all vehicles are projected down to a flat plane
+				bottomRight.y = 0; // Ensure all vehicles are projected down to the XZ plane
+				topRight = vec3{ bottomRight.x, 0, topLeft.z };
+				bottomLeft = vec3{ topLeft.x, 0, bottomRight.z };
 			}
 			
-			vec3 centerOfAllPlayers = (topLeft + bottomRight) / 2.0f;
+			vec3 centerOfAllPlayers = (topLeft + topRight + bottomLeft + bottomRight) / 4.0f;
 			vec3 cameraPos = centerOfAllPlayers + vec3{ 0, 100, 10 };
 			vec3 dirCameraToTopLeft = normalize(topLeft - cameraPos);
+			vec3 dirCameraToTopRight = normalize(topRight - cameraPos);
+			vec3 dirCameraToBottomLeft = normalize(bottomLeft - cameraPos);
 			vec3 dirCameraToBottomRight = normalize(bottomRight - cameraPos);
-			vec3 cameraForward = (dirCameraToTopLeft + dirCameraToBottomRight) / 2.0f;
+			vec3 cameraForward = (dirCameraToTopLeft + dirCameraToTopRight + dirCameraToBottomLeft + dirCameraToBottomRight) / 4.0f;
 			vec3 cameraLookAt = cameraPos + cameraForward;
+
+			//RenderSystem::drawDebugArrow(cameraPos + dirCameraToTopLeft * 2.0f, dirCameraToTopLeft, 50);
+			//RenderSystem::drawDebugArrow(cameraPos + dirCameraToTopRight * 2.0f, dirCameraToTopRight, 50);
+			//RenderSystem::drawDebugArrow(cameraPos + dirCameraToBottomLeft * 2.0f, dirCameraToBottomLeft, 50);
+			//RenderSystem::drawDebugArrow(cameraPos + dirCameraToBottomRight * 2.0f, dirCameraToBottomRight, 50);
+			//RenderSystem::drawDebugArrow(cameraPos + cameraForward * 2.0f, cameraForward, 50);
 
 			cameraEntity.camera.setLookAt(cameraPos, cameraLookAt, vec3{ 0, 1, 0 });
 		}
